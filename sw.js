49c57e3c0cec6bfc4cddfe67956da8fecdca1995
panckod91-lang <pancko.d91@gmail.com>
2026-05-09 19:30:25 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "panckobros-v76h-template-fix";
+const CACHE_NAME = "panckobros-v76i-viewer-text";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
